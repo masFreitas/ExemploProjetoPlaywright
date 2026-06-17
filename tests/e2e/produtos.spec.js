55e9d40c6274d69018ec17848eeee094002e59e3
MateusTestingCompany <mateus@testingcompany.com.br>
2026-06-17 11:57:34 -0300

    fix: ajuste pre-requisitos da tela de produtos

--- tests/e2e/produtos.spec.js
+++ tests/e2e/produtos.spec.js
@@ -8,6 +8,7 @@ test.describe('Produtos', () => {
 
     test.beforeEach(async ({ page }) => {
         await page.goto('/');
+        await basePage.acessarProdutos();
 
         basePage = new BasePage(page);
         productPage = new ProductPage(page);
@@ -15,19 +16,16 @@ test.describe('Produtos', () => {
 
 
     test('Realizar busca com sucesso', async ({ page }) => {
-        await basePage.acessarProdutos();
         await productPage.buscarProduto('Top');
         await productPage.validarBusca();
     });
 
     test('Validar tela de detalhes do produto', async ({ page }) => {
-        await basePage.acessarProdutos();
         await productPage.acessarPrimeiroProduto();
         await productPage.validarDetalhesProduto();
     });
 
     test('Enviar review de produto', async ({ page }) => {
-        await basePage.acessarProdutos();
         await productPage.acessarPrimeiroProduto();
         await productPage.validarDetalhesProduto();
         await productPage.escreverReviewProduto('Mateus', 'email@email.com', 'Ótimo produto, recomendo!');
